2c67e44fc5bee874d278561d750bf2c1f3653a25
clownlessmode <clownessmode@bk.ru>
2025-04-03 11:55:45 +0700

    fix

--- src/group/entity/group.entity.ts
+++ src/group/entity/group.entity.ts
@@ -2,13 +2,7 @@ import { DefaultEntity } from 'common/entities/default.entity';
 import { Curator } from 'src/curator/entities/curator.entity';
 import { Exam } from 'src/exams/entities/exam.entity';
 import { Student } from 'src/student/entity/student.entity';
-import {
-  Column,
-  Entity,
-  JoinColumn,
-  ManyToOne,
-  OneToMany,
-} from 'typeorm';
+import { Column, Entity, JoinColumn, ManyToOne, OneToMany } from 'typeorm';
 @Entity()
 export class Group extends DefaultEntity {
   @Column()
@@ -23,9 +17,9 @@ export class Group extends DefaultEntity {
   @JoinColumn()
   curator: Curator;
 
-  @OneToMany(() => Student, (student) => student.group)
+  @OneToMany(() => Student, (student) => student.group, { onDelete: 'CASCADE' })
   students: Student[];
 
-  @OneToMany(() => Exam, (exam) => exam.group)
+  @OneToMany(() => Exam, (exam) => exam.group, { onDelete: 'CASCADE' })
   exams: Exam[];
 }
